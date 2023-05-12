6384a2a95d1f50ada0edd7acfa14e42d539e7679
geogoucho Live <geogoucho@hotmail.de>
2023-05-12 10:53:29 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewTest;
+
+public partial class MainPage : ContentPage
+{
+     List <LapTimeClass> lapTimeList;
+    public class LapTimeClass
+    {
+        public TimeSpan start { get; set; }
+        public string EndTime { get; set; }
+        public string LapTime { get; set; }
+        public long LapCount { get; set; }
+    }
+
+    public MainPage()
+	{
+		InitializeComponent();
+        lapTimeList = new List<LapTimeClass>();
+        lapTimeList.Add(new LapTimeClass
+        {
+            LapTime = "asdf",
+            EndTime = "adsf",
+            LapCount = lapTimeList.Count,
+        });
+        lapTimeList.Add(new LapTimeClass
+        {
+            LapTime = "asdf",
+            EndTime = "adsf",
+            LapCount = lapTimeList.Count,
+        }); LapTimeView.ItemsSource = lapTimeList;
+
+    }
+
+
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CollectionViewTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
